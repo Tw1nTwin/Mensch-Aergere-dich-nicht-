230e08c1d1d407d7406abb8fc8d9589e3b79e2ce
Tw1nTwin <108219700+Tw1nTwin@users.noreply.github.com>
2022-07-17 19:43:13 +0200

    Testing api

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,11 @@
-fetch('https://raw.githubusercontent.com/Tw1nTwin/Mensch-Aergere-dich-nicht-/main/ReadMe.json')
-    .then(res => res.json())
+
+fetch("https://raw.githubusercontent.com/Tw1nTwin/Mensch-Aergere-dich-nicht-/main/ReadMe.json",{
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: "habub"})
+})
+.then(res => {
+    return res.json()
+    })
     .then(data => console.log(data))
+    .catch(error => console.log(error))
